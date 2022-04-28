4f5a760d4d12eb07740119001a5a831d424c0d17
Yevhen Galichenko <e.galichenko@gmail.com>
2022-04-28 16:56:54 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-
-let open = document.getElementById('open');
-let close = document.getElementById('close');
-let modal = document.getElementById('modal');
-
-open.addEventListener('click', function (event) {
-	event.preventDefault();
-	modal.style.display = 'block';
-});
+let open = document.getElementById('open')
+let close = document.getElementById('close')
+let modal = document.getElementById('modal')
 
 close.addEventListener('click', function (event) {
-	event.preventDefault();
-	modal.style.display = 'none';
-});
+	event.preventDefault()
+	modal.style.display = 'none'
+})
+
+open.addEventListener('click', function (event) {
+	event.preventDefault()
+	modal.style.display = 'block'
+})
